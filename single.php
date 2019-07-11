92e44cb121c4bec468fe74bdf8f51a9acbab1901
Kusuhara Jiei <>
2019-07-11 09:05:32 +0900

    singleのマージ

--- single.php
+++ single.php
@@ -1,18 +1,118 @@
-<?php
-/*
-Template name:汎用詳細ページ
-*/
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>beta</title>
+<meta name="viewport" content="width=device-width">
 
-get_header();
-?>
+<link rel="stylesheet" href="style/reset.css">
+<link rel="stylesheet" href="style/single.min.css">
+
+<script type="text/javascript" src="js/jquery.min.js"></script>
+<script type="text/javascript">
+</script>
 
 </head>
+
 <body>
-汎用ページ
+<!-------------------------------header&nav------------------------------>
+<header>
+	<ul class="top">
+		<li class="back">←</li>
+		<li class="title_name">アミューズメント</li>
+	</ul>
+</header>
+<!-------------------------------main------------------------------>
+<main>
+	<section>
+		<img src="images/x1.png" alt="404" class="shop_icon">
+		<h2>バッティングセンター</h2>
+		<h1>ATHLECITTA'蒲田</h1>
+		<p class="cost">￥600〜￥2000</p>
+		<p class="comment">総合評価</p>
+		<p class="point">★★★☆☆</p>
+		<p class="info">思いっきり楽しめて、安らげて、さらにフレッシュ！ 充実のアミューズメント&amp;リラクゼーション ボーリングやカラオケ、バッティングなど色々！</p>
+		<a href="" target="_blank">公式サイト</a>
+	</section>
+	<section>
+		<ul class="option">
+			<li class="free">バリアフリー</li>
+			<li class="gallery">ギャラリー</li>
+		</ul>
+		
+	</section>
+		
+		
+		<!--
+		<p class="sub"><span class="cost">￥9999~￥124580</span><span class="point">総合評価。。。。</span></p>
+		<h2>店舗情報</h2>
+		<ul class="shop_info">
+			<li>1</li>
+			<li>2</li>
+			<li>3</li>
+			<li>4</li>
+			<li>5</li>
+			<li>6</li>
+		</ul>
+		<p class="shop_info_p">texttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttext</p>
+		<h2>オススメポイント</h2>
+		<p class="shop_info_p">texttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttext</p>
+		<h2>公式サイト</h2>
+		<a href="" target="_blank">text</a>
+		-->
+	
+	<!--
+	<section>
+		<ul class="shop">
+			<li>バリアフリー</li>
+			<li>ポイント</li>
+		</ul>
+		<hr>
+		<table class="shop_table">
+			<tr>
+				<td class="table_left">店の広さ</td>
+				<td class="table_right">b</td>
+			</tr>
+			<tr>
+				<td class="table_left">注文形式</td>
+				<td class="table_right">b</td>
+			</tr>
+			<tr>
+				<td class="table_left">エレベーター</td>
+				<td class="table_right">b</td>
+			</tr>
+			<tr>
+				<td class="table_left">ピーク時の混雑度</td>
+				<td class="table_right">b</td>
+			</tr>
+			<tr>
+				<td class="table_left">店員さんの接客</td>
+				<td class="table_right">b</td>
+			</tr>
+		</table>
+	</section>
+	<section>
+		<ul class="shop">
+			<li>バリアフリー</li>
+			<li>ポイント</li>
+		</ul>
+		<hr>
+		<ul>
+			<li>a</li>
+			<li>a</li>
+			<li>a</li>
+			<li>a</li>
+			<li>a</li>
+			<li>a</li>
+			<li>a</li>
+		</ul>
+	</section>
+	<section>
+		<p>画像をタップしたら↓</p>
+		<img src="" alt="404">
+	</section>
+	-->
+</main>
 
-<?php get_footer()?>
-<!--このページ固有のScript-->
-
-<?php wp_footer(); ?>
 </body>
 </html>
